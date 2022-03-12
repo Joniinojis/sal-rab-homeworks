faf6700e7c8885e06f68a49fc29e692aa4d9fecf
joniinojis <joni.inojis@gmail.com>
2022-03-12 10:41:59 +0300

    Выполнение задания 1

--- task_1/solution.js
+++ task_1/solution.js
@@ -15,7 +15,11 @@ function addInCartMessage(title, price) {
     // выведите переменную message в консоль
 
     // Конец решения задания №1.1.
-
+    
+    let message = productName; 
+    message = message + " " + "за" + " " + productPrice + " " + "теперь в корзине!";
+    console.log(message);
+    
     return message;
 }
 
@@ -23,11 +27,12 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
-
+    let newValue = oldValue;
+        newValue = newValue + 1;
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
-
+        
     // Конец решения задания №1.2.
 
     return newValue;
@@ -38,7 +43,9 @@ function addInCartSumChange(sum, delta) {
     let difference = delta;
 
     // Задание №1.3. Увеличение суммы в корзине
-
+    let newSum = oldSum + difference;
+    let newSumText =  newSum + "₽";
+        newSumText = `${newSum} ₽`;
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
